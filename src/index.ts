66b2a5639cc8acbeaaaf7f11331ffb4be43873c0
afarroni <afarroni@heidelberg.edu>
2019-07-18 19:32:35 +0000

    option 3 complete

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,91 @@
 import './styles.css';
-import { add, PI } from './utils';
-console.log('Ready to Party');
+import { makeCurrency, percentFormat, validateInput } from './utils';
 
-console.log(add(10, 18));
+const buttons = document.querySelectorAll('.tip-percents button');
+const inputAmount: HTMLInputElement = document.querySelector('#amount');
+const customTip: HTMLInputElement = document.querySelector("#custTip");
+const numberOfPeople: HTMLInputElement = document.querySelector("#numPeople");
 
-console.log(PI);
+let numPeople = 0;
+let bill = 0;
+let tipPercentage: number;
+runApp();
+
+function runApp() {
+    setUp();
+}
+
+function setUp() {
+    initializeTipPercentage();
+
+    // set event listeners
+    customTip.addEventListener('blur', setCustomTip);
+    buttons.forEach(b => b.addEventListener('click', buttonPress));
+    inputAmount.addEventListener('input', calculateAndDisplayTableAmounts);
+    numberOfPeople.addEventListener('blur', calculateAndDisplayTableAmounts);
+}
+
+function buttonPress() {
+    const that: HTMLButtonElement = this;
+    // change active button
+    buttons.forEach((b: HTMLButtonElement) => {
+        if (b.disabled) {
+            b.disabled = false;
+        }
+    });
+    tipPercentage = this.dataset.amount;
+    localStorage.setItem("tipPercentage", tipPercentage.toString());
+    displayTipPercentage();
+    calculateAndDisplayTableAmounts();
+};
+
+function calculateAndDisplayTableAmounts() {
+    validateAndSetInput();
+    // calculate 
+    let tipAmount = bill * tipPercentage;
+    let totalBill = bill + tipAmount;
+    let splitBill = bill / numPeople;
+    // display
+    document.getElementById("bill").innerHTML = makeCurrency(bill);
+    document.getElementById("tip-amount").innerHTML = makeCurrency(tipAmount);
+    document.getElementById('total-bill').innerHTML = makeCurrency(totalBill);
+    if (numPeople > 0) {
+        document.getElementById('per-person').innerHTML = makeCurrency(splitBill);
+    } else {
+        document.getElementById('per-person').innerHTML = "";
+    }
+}
+
+function validateAndSetInput() {
+    bill = validateInput(inputAmount);
+    numPeople = validateInput(numberOfPeople);
+}
+
+function setCustomTip() {
+    const that = this;
+    let customTipAmount = parseFloat(that.value) / 100;
+    document.getElementById('custButton').dataset.amount = customTipAmount.toString();
+    displayTipPercentage();
+}
+
+function initializeTipPercentage() {
+    // get tip percentage from storage, or set to .20
+    if (typeof (Storage) !== "undefined") {
+        tipPercentage = parseFloat(localStorage.getItem("tipPercentage"));
+    } else {
+        tipPercentage = .20;
+    }
+    displayTipPercentage();
+}
+
+function displayTipPercentage() {
+    buttons.forEach((b: HTMLButtonElement) => {
+        if (tipPercentage == parseFloat(b.dataset.amount)) {
+            if (b.id !== "custButton") {
+                b.disabled = true;
+            }
+            document.getElementById("description").innerHTML = `You are tipping ${percentFormat(b.dataset.amount)}.`;
+            document.getElementById("tip-percent").innerHTML = percentFormat(b.dataset.amount);
+        }
+    });
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,25 @@
 
-export function add(a, b) {
-    return a + b;
+export function makeCurrency(num: number): string {
+    if (isNaN(num)) {
+        return '$0';
+    }
+    else {
+        return '$' + `${num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
+    }
 }
 
+export function percentFormat(dec: string): string {
+    return `${Math.round(parseFloat(dec) * 100)}%`;
+}
 
-export const PI = 3.1415;
-
-// window["add"] = add;
+export function validateInput(that: HTMLInputElement): number {
+    // check if a negative is attempted
+    if (that.value.indexOf('-') > -1) {
+        that.classList.add('alert-danger');
+        return 0;
+    }
+    else {
+        that.classList.remove("alert-danger");
+        return parseFloat(that.value);
+    }
+}
